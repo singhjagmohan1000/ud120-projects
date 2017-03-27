ce732a92983c1b1aedb95c7fb166427ea1a8deb7
Jagmohan Singh <jagmohan.singh@sjsu.edu>
2017-03-27 01:41:28 -0700

    udacity naive-bayes mini project completed

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -9,7 +9,7 @@
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -27,6 +27,19 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+
+from sklearn.naive_bayes import GaussianNB
+import time
+clf = GaussianNB()
+tick = time.time()
+clf.fit(features_train,labels_train)
+print ("Time in training: ", time.time()-tick)
+GaussianNB(priors=None)
+
+testtime = time.time()
+print (clf.predict(features_test))
+print ("Time in prediction: ", time.time()-testtime)
+print (clf.score(features_test,labels_test))
 
 #########################################################
 
